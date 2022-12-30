bc0cfca5259de29f96e53b48b717f349233bb6e3
jesse berkowitz <jberkow@brandeis.edu>
2022-12-29 16:20:13 -0800

    get function

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ class Post(BaseModel):
     content: str
     rating: Optional[int]
 
+my_posts = [{"title":"title_1", "content":"content_1", "rating":5},
+{"title":"title_2", "content":"content_2", "rating":3}]
 
 @app.get('/')
 def home():
     return 'You are home now'
+
+@app.get('/post')
+def get_post():
+    return {"data": my_posts}
 
 @app.post('/post')
 def create_post(post:Post):
