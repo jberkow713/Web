2758b07b3763b3f4dfe9f2f24c2b27f01da20e6f
jesse berkowitz <jberkow@brandeis.edu>
2022-12-29 17:43:01 -0800

    updated post functions

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ class Post(BaseModel):
     title: str
     content: str
     rating: Optional[int]
+    id: int
 
-my_posts = [{"title":"title_1", "content":"content_1", "rating":5},
-{"title":"title_2", "content":"content_2", "rating":3}]
+my_posts = [{"title":"title_1", "content":"content_1", "rating":5, "id":1},
+{"title":"title_2", "content":"content_2", "rating":3, "id":2}]
 
+def Get_post(id):
+    id = int(id)
+    for p in my_posts:
+        if p["id"]==id:
+            return p 
+    return 'post_id not found'
 @app.get('/')
 def home():
     return 'You are home now'
@@ -25,4 +32,15 @@ def get_post():
 def create_post(post:Post):
     print(post.title)
     return post
+
+@app.get("/post/latest")
+def get_latest():
+    return {"latest_post": my_posts[len(my_posts)-1]}
+
+@app.get("/post/{id}")
+def get_post(id):
+    post = Get_post(id)
+    return {"post_detail":post}
+
+
     
