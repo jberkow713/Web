a26756267c3e32b7e4324a5c261db1490e1d1487
jesse berkowitz <jberkow@brandeis.edu>
2023-02-11 13:46:51 -0800

    dictionary flatten ideas

---
+++ recursion.py
@@ -0,0 +1,113 @@
+
+
+d = {
+  "items": [
+    {
+      "id": "12345",
+      "links": {
+        "self": "https://www.google.com"
+      },
+      "name": "beast",
+      "type": "Device"
+    }
+  ],
+  "links": {
+    "self": "https://www.google.com"
+  },
+  "paging": {
+    "count": 1,
+    "limit": 1,
+    "offset": 0,
+    "pages": [{'k':1, 'v':[1,2,3,[4,5,[6,[8]]]], 'f':{'f':[1,2,3,4,5,[3,4]]}}]
+  }
+}
+
+d2 = {
+    "id": 1,
+    "class": "c1",
+    "owner": "myself",
+    "metadata": {
+        "m1": {
+            "value": "m1_1",
+            "timestamp": "d1"
+        },
+        "m2": {
+            "value": "m1_2",
+            "timestamp": "d2"
+        },
+        "m3": {
+            "value": "m1_3",
+            "timestamp": "d3"
+        },
+        "m4": {
+            "value": "m1_4",
+            "timestamp": "d4"
+        }
+    },
+    "a1": {
+        "a11": [
+
+        ]
+    },
+    "m1": {},
+    "comm1": "COMM1",
+    "comm2": "COMM21529089656387",
+    "share": "xxx",
+    "share1": "yyy",
+    "hub1": "h1",
+    "hub2": "h2",
+    "context": [{'f':[1,2,3,[4,5]]}
+
+    ]
+}
+
+def flatten_list(List):
+    L = []
+    for val in List:
+        if isinstance(val, list):
+            output = flatten_list(val)
+            for x in output:
+                L.append(x)
+        else:
+            L.append(val)
+    return L
+
+def flatten_dict(Dict, sep=''):
+    Final = {}
+
+    for k,v in Dict.items():
+        key = sep+k 
+        if isinstance(v,dict):
+            output = flatten_dict(v, key + '.')
+            if output == {}:
+                Final[key]={}
+            else:
+                Final.update(output)
+
+        elif isinstance(v,list):
+            v = flatten_list(v)
+            l = len(v)
+            count = 0
+            for x in v:
+                if type(x)==dict:
+                    count +=1
+            if count == 0:                
+                Final[key]=v
+                
+            for i in v:                               
+                if isinstance(i, dict):                   
+                    Final.update(flatten_dict(i,key+'.'))
+        else:
+            Final[key]=v
+    return Final             
+
+d3 = {
+    "count": 1,
+    "limit": 1,
+    "offset": 0,
+    "pages": [{'k':1, 'v':[1,2,3,[4,5,[6,[8]]]]}]
+  }
+
+print(flatten_dict(d))
+
+
